3322f355a22e79d3b96ce496dd80a2a1404b02f8
rizwanAli <rizwanali4574992@gmail.com>
2024-06-28 23:00:43 +0500

    react first project

---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <div>
+     <nav>Dawn</nav>
+     <h1>Israel's Gaza invasion - Day 265</h1>
+     <main>
+        <div className="left">
+            <ul>
+                <li>Israel’s siege of Gaza Strip, sparked by Hamas’ Oct 7 attack, enters ninth month</li>
+                <li>Israeli strikes have laid waste to much of Gaza strip and caused major environmental damage</li>
+                <li>Starving Palestinians surviving on less than 3pc of daily water requirements while 67pc of water, sanitation infrastructure destroyed</li>
+                <li>More than 250 killed in Israeli raid on Nuseirat refugee camp to rescue 4 hostages, days after a strike on UN school killed at least 35</li>
+                <li>Truce not secured despite UNSC adopting resolution based on ceasefire plan announced by Biden</li>
+            </ul>
+        </div>
+        <div className="right">
+            <img src="https://react-first-class1.netlify.app/assets/main-DOJG83t2.jpg" alt="" />
+            <h4>Creat by Rizwan Ali</h4>
+        </div>
+     </main>
+      </div>
+  );
+}
+
+export default Home;
